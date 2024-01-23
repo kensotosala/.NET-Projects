902aa005dcc254582ffe91b1060f98a610c09cf8
Your Name <you@example.com>
2024-01-23 08:58:40 -0600

    AppCrud Removed

--- AppCrud/AppCrud/Models/Departamento.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CRUD_ASP.NET_MVC_ADO.NET.Models
-{
-    public class Departamento
-    {
-        public int idDepartamento { get; set; }
-        public string nombre { get; set; } = string.Empty;
-    }
-}
